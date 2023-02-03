72790cf8367da1da51065df14be11b58e67ae27c
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-02-03 20:05:25 +0200

    task2+

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listOfIngredientsEl = document.querySelector('ul#ingredients');
+
+let fragment = new DocumentFragment();
+
+for (const value of ingredients) {
+  const itemOfIngredientsEl = document.createElement('li');
+  itemOfIngredientsEl.classList.add('item');
+  itemOfIngredientsEl.textContent += value;
+  fragment.appendChild(itemOfIngredientsEl);
+}
+
+document.querySelector('ul#ingredients').append(fragment);
