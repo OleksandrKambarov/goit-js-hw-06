6bffc9f31ca04a08efdbfe384e4e18be079e366b
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-02-05 20:03:13 +0200

    task8+

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const manageForm = document.querySelector('.login-form');
+manageForm.addEventListener('submit', submitForm);
+const profile = {};
 
+function submitForm (event) {
+  event.preventDefault();
+  
+  const email = event.currentTarget.elements.email;
+  const password = event.currentTarget.elements.password;
+
+  if (email.value === '' || password.value === '') {
+    alert('Please fill in all the fields!');
+  } else {
+    profile.email = email.value;
+    profile.password = password.value;
+
+    console.log(profile);
+  }
+  event.currentTarget.reset();
+}
