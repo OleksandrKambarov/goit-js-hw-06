31eff39b3cfbbfcae46e032cb0d30f0092fc7d7c
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-02-05 22:59:48 +0200

    task6

--- js/task-06.js
+++ js/task-06.js
@@ -7,3 +7,25 @@ textInput.addEventListener("blur", (event) => {
   }
   return textInput.classList.add("invalid");
 });
+
+const inputRef = document.getElementById("validation-input");
+inputRef.addEventListener("blur", onBlurBorderColor);
+
+// var1
+function onBlurBorderColor(event) {
+  let inputValue = event.currentTarget.value;
+  if (inputValue.length === Number(inputRef.dataset.length)) {
+    inputRef.classList.add("valid");
+    inputRef.classList.remove("invalid");
+  }
+  if (inputRef.value.length === 0) {
+    inputRef.classList.remove("valid");
+    inputRef.classList.remove("invalid");
+  }
+  if (
+    inputRef.value.length !== Number(inputRef.dataset.length) &&
+    inputRef.value.length !== 0
+  ) {
+    inputRef.classList.add("invalid");
+  }
+}
