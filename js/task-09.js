dc1c37188f69a45e79baf84c77cfdbfb87e1ba97
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-02-05 20:13:02 +0200

    task9+

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const textColor = document.querySelector('.color');
+const changeFonColorBtn = document.querySelector('.change-color');
+const body = document.body;
+
+const changeFonColor = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  textColor.textContent = getRandomHexColor();
+};
+
+changeFonColorBtn.addEventListener('click', changeFonColor);
