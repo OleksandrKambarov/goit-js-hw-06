f478f78b57ed55723336c29d4f658089c6537099
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-02-05 12:16:58 +0200

    task7++

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,7 @@
-const inputEl = document.querySelector("#font-size-control");
-const textEl = document.querySelector("#text");
+const inputEl = document.getElementById('font-size-control');
+const textEl = document.getElementById('text');
 
-inputEl.addEventListener("input", (event) => {
-    console.dir(`${event.currentTarget.value}px`);
-    textEl.classList.add("fontsize");
-    textEl.style.fontSize === `${event.currentTarget.value}px`;
-})
+const fontSizeControl = ({ currentTarget }) =>
+  (textEl.style.fontSize = `${currentTarget.value}px`);
+
+inputEl.addEventListener('input', fontSizeControl);
