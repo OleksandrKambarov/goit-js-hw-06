6b67c9823b41ac449f4b1154e2eaca24d0f4fd97
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-02-06 15:11:14 +0200

    task6-add-remove

--- js/task-06.js
+++ js/task-06.js
@@ -1,20 +1,11 @@
 const textInput = document.getElementById("validation-input");
-textInput.addEventListener("blur", onBlurColor);
-
-function onBlurColor(event) {
-  let inputValue = event.currentTarget.value;
-  if (inputValue.length === Number(textInput.dataset.length)) {
-    textInput.classList.add("valid");
-    textInput.classList.remove("invalid");
-  }
-  if (textInput.value.length === 0) {
-    textInput.classList.remove("valid");
-    textInput.classList.remove("invalid");
-  }
-  if (
-    textInput.value.length !== Number(textInput.dataset.length) &&
-    textInput.value.length !== 0
-  ) {
-    textInput.classList.add("invalid");
-  }
+textInput.addEventListener("blur", event => {
+  if (textInput.value.length === Number(textInput.dataset.length)) {
+  textInput.classList.remove("invalid");
+  return textInput.classList.add("valid");
 }
+textInput.classList.remove("valid");
+return textInput.classList.add("invalid");
+});
+
+
